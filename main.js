04f679c4cff31f1a5c380086be726be2b7e14032
david-laier <54219606+djl1899@users.noreply.github.com>
2026-01-27 21:32:24 +0100

    Implement infinite slider functionality
    Added functionality for infinite slider by cloning slides.

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ function toggleMenu() {
   const menu = document.getElementById("menu-overlay");
   menu.style.display = menu.style.display === "flex" ? "none" : "flex";
 }
+
+// INFINITE SLIDER
+const track = document.querySelector('.slider-track');
+if (track) {
+  const slides = Array.from(track.children);
+  slides.forEach(slide => {
+    const clone = slide.cloneNode(true);
+    track.appendChild(clone);
+  });
+}
